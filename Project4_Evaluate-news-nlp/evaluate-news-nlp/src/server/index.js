88df2e6846a5fbfb1079d29a419f77ae8c97b5ba
jerryzhang2019 <engineerjerry1@gmail.com>
2020-06-26 23:06:22 -0700

    upload my local code to server

---
+++ Project4_Evaluate-news-nlp/evaluate-news-nlp/src/server/index.js
@@ -0,0 +1,53 @@
+var path = require('path')
+const express = require('express')
+const mockAPIResponse = require('./mockAPI.js')
+
+var aylien = require('aylien_textapi');
+const bodyParser = require('body-parser');
+
+require('dotenv').config()
+
+const textapi = new aylien({
+    appilication_id: `${process.env.API_ID}`,
+    appilication_key:`${process.env.API_KEY}`
+});
+
+const app = express()
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+app.use(express.static('dist'))
+
+console.log(__dirname)
+
+app.get('/', function (req, res) {
+    // res.sendFile('dist/index.html')
+    res.sendFile(path.resolve('dist/index.html'))
+})
+
+// designates what port the app will listen to for incoming requests
+app.listen(8081, function () {
+    console.log('Example app listening on port 8081!')
+})
+
+app.post('/testing',async(req, res, next) =>{
+    console.log(req.body);
+    try{
+        var data=textapi.sentiment({
+            'text':req.body.theText
+        },function(error,response){
+            if(error === null){
+                console.log(response);
+                res.send(response);
+            }
+        });
+
+    }catch(error){
+        return next(error)
+    }
+})
+
+app.get('/test', function (req, res) {
+    res.send(mockAPIResponse)
+})
---
+++ Project4_Evaluate-news-nlp/evaluate-news-nlp/webpack.prod.js
@@ -0,0 +1,39 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebPackPlugin = require("html-webpack-plugin")
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const workboxPlugin = require('workbox-webpack-plugin');
+
+module.exports = {
+    entry: './src/client/index.js',
+    mode: 'production',
+    optimization:{
+      minimizer:[new TerserPlugin({}), new OptimizeCSSAssetPlugin({})],  
+    },
+    module: {
+        rules: [
+            {
+                test: '/\.js$/',
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test:/\.scss$/,
+                use:[MiniCssExtractPlugin.loader, 'css-loader','sass-loader']
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html",
+        }),
+        new MiniCssExtractPlugin({filename:'[name].css'}),
+        new workboxPlugin.GenerateSW({
+            clientsClaim:true,
+            skipWaiting:true
+        }),
+    ]
+}
