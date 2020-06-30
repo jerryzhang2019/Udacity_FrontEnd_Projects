264899a3363e014814e5659e6d78ce8625eae3c0
jerryzhang2019 <engineerjerry1@gmail.com>
2020-06-29 18:04:33 -0700

    create webpack function

---
+++ Project5_Travel_App/webpack.dev.js
@@ -0,0 +1,27 @@
+const path=require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const common = require('./webpack.common')
+const merge = require('webpack-merge')
+
+module.exports=merge(common,{
+    mode:'development',
+    devServer:{
+        port:8000,
+    },
+    watch:true,
+    plugins:[
+        new HtmlWebpackPlugin({
+            template:'./src/client/views/index.html'
+        })
+    ],
+    module:{
+        rules:[
+            {
+                test:/\.scss$/,
+                use:['style-loader',
+                    'css-loader',
+                    'sass-loader']
+            }
+        ]
+    }
+});
