42eae7ab76d6d89f0beafa1724cef6bfd69556d4
jerryzhang2019 <engineerjerry1@gmail.com>
2020-07-14 10:29:19 -0700

    debug code

--- Project5_Travel_App/webpack.dev.js
+++ Project5_Travel_App/webpack.dev.js
@@ -1,7 +1,7 @@
 const path=require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const common = require('./webpack.common')
-const merge = require('webpack-merge')
+const {merge} = require('webpack-merge')
 
 module.exports = merge(common,{
     mode:'development',
