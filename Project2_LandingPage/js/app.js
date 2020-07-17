c4893f2e77d1af818d5bd6b368a69633640404d2
jerryzhang2019 <engineerjerry1@gmail.com>
2020-07-16 17:14:34 -0700

    modify my js code to server

--- Project2_LandingPage/js/app.js
+++ Project2_LandingPage/js/app.js
@@ -19,6 +19,7 @@
 */
 let sectionList = document.querySelectorAll('section');
 let navListTag = document.getElementById('navbar_list');
+
 let sectionLength = sectionList.length;
 let sectionPositions = [];
 let oldPosition = 0;
@@ -42,37 +43,36 @@ function scrollToSection(sectionID){
 // build the nav建立导航
 sectionList.forEach((element, index) => {
     let sectionName = element.getAttribute('data-nav');
-    let toOffSection = element.offsetTop+30;
+    let toOffSection = element.offsetTop + 30;
     let liTag = document.createElement('li');
     liTag.setAttribute('class', 'menu_link' + index);
-    liTag.innerHTML = `<a onClick = "scrollToSection(${toOffSection})">${sectionName}</a>`;
+    liTag.innerHTML = `<a onClick="scrollToSection(${toOffSection})">${sectionName}</a>`;
     navListTag.appendChild(liTag);
-});
+  });
 
 // add event listener
 document.addEventListener('scroll', () => {
     currentPosition = this.scrollY;
-
+    // Section Positions
     sectionPositions = [];
     sectionList.forEach(element =>
-        sectionPositions.push(element.getBoundingClientRect().top + 50));
-
-//
-    let addIndex = sectionPositions.findIndex(element => element >0);
-    for (let i = 0; i < sectionLength; i++){
-        if (addIndex === i){
-            document.querySelector('.menu_link' + addIndex).classList.add('active');
-            document
-                .querySelector(`#section${addIndex + 1}`)
-                .classList.add('highlight');
-
-        }else{
-            document.querySelector('.memu_link'+i).classList.remove('active');
-            document.querySelector(`#section${i+1}`).removeAttribute('class');
-        }
-
+      sectionPositions.push(element.getBoundingClientRect().top + 50)
+    );
+  
+    // Adding and removing active sections
+    let addIndex = sectionPositions.findIndex(element => element > 0);
+    for (let i = 0; i < sectionLength; i++) {
+      if (addIndex === i) {
+        document.querySelector('.menu_link' + addIndex).classList.add('active');
+        Document
+          .querySelector(`#section${addIndex + 1}`)
+          .classList.add('highlight');
+      } else {
+        document.querySelector('.menu_link' + i).classList.remove('active');
+        document.querySelector(`#section${i + 1}`).removeAttribute('class');
+      }
     }
-});
+  });
 
 
 
