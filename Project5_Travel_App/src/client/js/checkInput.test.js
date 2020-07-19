3f3b1f6988f235d906ec635ac9af70e2b49578d5
jerryzhang2019 <engineerjerry1@gmail.com>
2020-07-19 00:50:44 -0700

    install jest for project5

--- Project5_Travel_App/src/client/js/checkInput.test.js
+++ Project5_Travel_App/src/client/js/checkInput.test.js
@@ -1,9 +1,9 @@
-import {checkUserInput} from './checkInput.js';
+import { checkUserInput } from './checkInput.js';
 
-describe('RegExp: input', function (){
-    it(' should be a string', function (){
-        const urlRGEX = /^[a-zA-Z\s]{0,255}$/;
-        const urlTest = 'L0ond0n';
-        expect(urlRGEX.test(urlTest)).toBe(false);
-    });
+describe('RegExp: input', function () {
+  it('should be a string', function () {
+    const urlRGEX = /^[a-zA-Z\s]{0,255}$/;
+    const urlTest = 'L0nd0n';
+    expect(urlRGEX.test(urlTest)).toBe(false);
+  });
 });
