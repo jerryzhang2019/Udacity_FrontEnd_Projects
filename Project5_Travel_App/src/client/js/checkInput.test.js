df3b80e500ecec54026e9a7ac1ec3c26c849b1c8
jerryzhang2019 <engineerjerry1@gmail.com>
2020-07-19 00:21:49 -0700

    update my code of project 5

--- Project5_Travel_App/src/client/js/checkInput.test.js
+++ Project5_Travel_App/src/client/js/checkInput.test.js
@@ -1,9 +1,9 @@
-import {checkUserInput} from './checkInput';
+import {checkUserInput} from './checkInput.js';
 
 describe('RegExp: input', function (){
     it(' should be a string', function (){
         const urlRGEX = /^[a-zA-Z\s]{0,255}$/;
-        const urlTest = 'Seattl0';
+        const urlTest = 'L0ond0n';
         expect(urlRGEX.test(urlTest)).toBe(false);
     });
 });
